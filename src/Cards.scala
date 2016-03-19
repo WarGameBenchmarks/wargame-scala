fc57c5a7f01432f8b600305e634071c440499a8c
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-03-19 01:07:01 -0500

    Added comments

--- src/Cards.scala
+++ src/Cards.scala
@@ -1,18 +1,44 @@
 object Cards {
 
+
+  /**
+   * Enum for Card Values.
+   *
+   * Note: in Scala, there are two choices for handling enumerations.
+   * An `object` can be extended from Enumeration, in which it functions
+   * similarly to Java's `enum`.
+   *
+   * The other case, incidentially, is a barrage of `case object`s.
+   * This is also similar to Java's `enum` extension, which provides methods
+   * and properties for enum values, but this results in a boat load
+   * of complexity plus many more compiled classes.
+   *
+   */
   object CardValue extends Enumeration {
     type CardValue = Value
     val Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
         Jack, Queen, King, Ace = Value
   }
+
+  // Allow these to exist freely.
   import CardValue._
 
+  /**
+   * Enum for Card Suits.
+   */
   object CardSuit extends Enumeration {
     type CardSuit = Value
     val Hearts, Diamonds, Spades, Clubs = Value
   }
+
+  // Allow these to exist freely.
   import CardSuit._
 
+  /**
+   * Return a CardValue's string name.
+   * @param  {CardValue} v: CardValue     a card value
+   * @return {String}    the name of the card value
+   */
   def valueToString(v: CardValue): String = v match {
     case CardValue.Two => "Two"
     case CardValue.Three => "Three"
@@ -30,6 +56,11 @@ object Cards {
     case _ => "?"
   }
 
+  /**
+   * Return a CardValue's integer value.
+   * @param  {CardValue} v: CardValue     a card value
+   * @return {Int}    the integer representation of the card value
+   */
   def valueToInt(v: CardValue): Int = v match {
     case CardValue.Two => 2
     case CardValue.Three => 3
@@ -47,6 +78,11 @@ object Cards {
     case _ => 0
   }
 
+  /**
+   * Return a CardSuit's name.
+   * @param  {CardSuit} s: CardSuit      a card suit
+   * @return {String}    a name of the Card Suit
+   */
   def suitToString(s: CardSuit): String = s match {
     case CardSuit.Hearts => "Hearts"
     case CardSuit.Spades => "Spades"
@@ -55,6 +91,9 @@ object Cards {
     case _ => "?"
   }
 
+  /**
+   * Card, typically represented by a value and suit. Allows for comparison.
+   */
   class Card(v: CardValue, s: CardSuit) extends Ordered[Card] {
     var value = v
     var suit = s
@@ -65,6 +104,8 @@ object Cards {
   }
 
   import scala.collection.mutable.ArrayBuffer
+
+
   class Deck(c: ArrayBuffer[Card]) {
     var cards: ArrayBuffer[Card] = c
 
@@ -83,6 +124,10 @@ object Cards {
   }
 
   object Deck {
+    /**
+     * Generate a fresh deck with the 52 card, 4 suit of 13 cards each setup.
+     * @return {Deck} return a deck of 52 cards
+     */
     def fresh(): Deck = {
       val deck = new Deck()
       val suits = List(Hearts, Diamonds, Clubs, Spades)

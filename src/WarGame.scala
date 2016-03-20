b9f0ec3751b3b5e3e6c4251080de0057abe54bef
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-03-20 01:07:49 -0500

    Added initial WarGame file

---
+++ src/WarGame.scala
@@ -0,0 +1,19 @@
+import Card._
+import Deck._
+
+class WarGame {
+
+  def play() {
+
+    val deck = Deck.fresh()
+    deck.shuffle()
+
+    val (c1, c2) = deck.split()
+
+    val winner = new Deck()
+
+
+
+  }
+
+}

cf4b76881015e4957c65cc366d43684add9f2a45
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-03-24 00:51:12 -0500

    Added comments
    I know I won't remember

--- src/Benchmark.scala
+++ src/Benchmark.scala
@@ -24,6 +24,10 @@ class Benchmark {
     def receive = {
       case "start" => {
         println("game actor: started")
+        /**
+         * This causes an infinite loop.
+         * Weep with me.
+         */
         while (running) {
           game.play()
           sender ! "progress"
@@ -31,6 +35,11 @@ class Benchmark {
       }
       case "stop" => {
         println("game actor: stopping")
+        /**
+         * `running` = false should stop
+         * the loop above from running, but it does not.
+         * Plus, it should end itself.
+         */
         running = false
         context.stop(self)
       }
@@ -49,26 +58,45 @@ class Benchmark {
     def receive = {
       case "start" => {
         println("started")
+        /**
+         * Start each GameActor
+         */
         for (actor <- actors) {
           actor ! "start"
         }
       }
       case "progress" => {
-          total += 1
-          println("progress: " + total)
+        /**
+         * When a GameActor makes progress,
+         * accumulate the progress here
+         */
+        total += 1
+        println("progress: " + total)
 
-          if (total > 2000) {
-            for (actor <- actors) {
-              actor ! "stop"
-            }
-            println("stopping")
+        /**
+         * This should trigger an end point during testing.
+         * It's disgusting.
+         *
+         * In theory, this should call `stop` on
+         * each GameActor but for some reason, it never triggers.
+         */
+        if (total > 2000) {
+          for (actor <- actors) {
+            actor ! "stop"
           }
+          println("stopping")
+        }
       }
       case "stop" => {
-          stopped += 1
-          if (stopped >= actors.length) {
-            context.stop(self)
-          }
+        /**
+         * Each GameActor should respond that it stopped.
+         * And this `Master` should handle
+         * its own shutdown when all of the games are done.
+         */
+        stopped += 1
+        if (stopped >= actors.length) {
+          context.stop(self)
+        }
       }
       case "error" => {
         println("error")
@@ -83,14 +111,18 @@ class Benchmark {
   def benchmark() {
     val system = ActorSystem("BenchmarkSystem")
 
+    // pretend for now to use a 2-thread system
     val count:Int = 2
     val actors = new Array[ActorRef](count)
 
+    // make N actors
     for (i <- 0 until count) {
       val actor = system.actorOf(Props(new GameActor), name = "GameActor" + i)
       actors(i) = actor
     }
 
+    // make the Master actor (or main thread?) and pass it the
+    // thread game actors
     val master = system.actorOf(Props(new Master(actors)), name = "Master")
 
     master ! "start"
